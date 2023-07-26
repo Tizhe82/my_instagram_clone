bfe40e41bbb1dc14171655b41cb19d6bdd19a906
Tizhe82 <tizhepaul@gmail.com>
2023-07-26 15:15:53 +0100

    added firestore && lib folder

--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render( <App />);
-
-// client side rendered app: react (create react app)
-// -> database which is Firebase
-// -> react-loading-skeleton
-// tailwind
-
-// architecture
-// -> components,Immutables,conditions,support,lib,services,styles
+import FirebaseContext from './conditions/firebase';
 
 
+ReactDOM.render(
+<FirebaseContext.Provider value={{firebase, FiedValue}}>
+    <App/>
+</FirebaseContext.Provider>,  
+document.getElementById('root'));
